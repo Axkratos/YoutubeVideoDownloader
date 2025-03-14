dd7b23bb897ab25470bc4fc2c6a0b95c4a298141
Anup Adhikari <anupadhikari269@gmail.com>
2025-03-14 22:01:28 +0545

    first commit

--- app.py
+++ app.py
@@ -237,7 +237,10 @@ def download_video():
                 'key': 'FFmpegVideoConvertor',
                 'preferedformat': 'mp4',
             }],
-        }
+            'headers': {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+            }
+}
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
